c0be5112fbca1db13323ff2296d297bc17344cfb
Zel <jules.hermyer@gmail.com>
2022-08-21 11:49:47 +0200

    added path reconstruction

--- graph_impl/main.ml
+++ graph_impl/main.ml
@@ -8,17 +8,25 @@ let inf = 99999
 let floyd_warshall g =
 	let n = Array.length g.vtx - 1 in
 	let dist = Array.make_matrix (n+1) (n+1) 0 in
+	let next = Array.make_matrix (n+1) (n+1) None in
 	for i = 0 to n do
 		for j = 0 to n do
-			dist.(i).(j) <- g.edges.(i).(j)
+			dist.(i).(j) <- g.edges.(i).(j);
+			next.(i).(j) <- Some j
 		done
+	done;
+	for i = 0 to n do
+		dist.(i).(i) <- 0;
+		next.(i).(i) <- Some i;
 	done;
 	for k = 0 to n do
 		for i = 0 to n do
 			for j = 0 to n do
 				if dist.(i).(k) + dist.(k).(j) 
 					< dist.(i).(j)
-					then dist.(i).(j) <- dist.(i).(k) + dist.(k).(j)
+					then begin dist.(i).(j) <- dist.(i).(k) + dist.(k).(j);
+								next.(i).(j) <- next.(i).(k)
+						end
 			done
 		done
 	done;
